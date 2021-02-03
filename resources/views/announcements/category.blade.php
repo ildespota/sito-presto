0c8f0e5b4c861a591dacb1b898c0218fd7192c9c
Karin Latino <karin@192.168.1.54>
2021-02-03 19:03:00 +0100

    components card

--- resources/views/announcements/category.blade.php
+++ resources/views/announcements/category.blade.php
@@ -1,26 +1,27 @@
-<x-layout title="ciao">
-<div class="container my-5">
-    <div class="row">
+<x-layout title="Categorie">
+<div class="container-fluid my-5">
+    <div class="row justify-content-around mx-2 ">
       
 
-        @foreach ($announcements as $announcement)
-        <div class="col-12 col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://picsum.photos/200" class="card-img-top img-fluid" alt="{{$announcement->title}}">
-          <div class="card-body">
-            <h5 class="card-title">{{$announcement->title}}</h5>
-            <p class="card-text">{{$announcement->description}}</p>
-            <p class="card-text">{{$announcement->price}}</p>
-            <p class="card-text">{{$announcement->created_at->format('d-m-Y')}}</p>
-            <p class="card-text">{{$announcement->user->name}}</p>
-            <a href= "{{route('announcement.category', $announcement->category) }}" class="card-text">{{$announcement->category->name}}</a>
-            <a href="{{route('announcement.show', $announcement)}}" class="btn btn-primary">Dettaglio</a>
-          </div>
-        </div>
+      @foreach ($announcements as $announcement)
+      <x-card
+      announcement="{{$announcement->id}}"
+      title="{{$announcement->title}}"
+      description="{{$announcement->description}}"
+      createdAt="{{$announcement->created_at->format('d-m-Y')}}"
+      name="{{$announcement->category->name}}"
+      price="{{$announcement->price}}"
+      category="{{$announcement->category->id}}"
+      />
+      
+      @endforeach
+
+      
+    </div>
+    <div class="row mt-4">
+      <div class="d-flex justify-content-center">
+        {!! $announcements->links() !!}
       </div>
-        @endforeach
-
-      
     </div>
   </div>
   
